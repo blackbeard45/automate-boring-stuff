7d094e046c1639f5d6663b444fbc8f7cabb1f889
blackbeard45 <vilhoerno@gmail.com>
2025-09-20 06:57:50 +0330

    solve the second part of te exercise of chapter 5.

--- chapter_05/fantasy_game_inventory.py
+++ chapter_05/fantasy_game_inventory.py
@@ -6,5 +6,15 @@ def display_inventory(inventory):
         total_items += v
     print(f"Total number of items: {total_items}")
 
+def add_to_inventory(inventory, added_items):
+    for item in added_items:
+        if item in inventory:
+            inventory[item] += 1
+        else:
+            inventory[item] = 1
+            
+dragons_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+add_to_inventory(inventory, dragons_loot)
 display_inventory(inventory)
